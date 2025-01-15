9c80b0030e19b762336de6fd2d92a9b19c2bb7c6
fengdachao <fengdachao@aliyun.com>
2025-01-15 17:47:55 +0800

    configuration

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { v4 as uuidv4 } from 'uuid'
 import { HOST } from './constant'
 
-// const domain = 'http://mongo-local:3001'
+// const domain = 'http://api-server:3001'
 const domain = `http://${HOST}:3001`
 
 export const getList = async (params) => {
