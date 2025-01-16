152207f99d30594c98b0c6f6b3ef916a0562e20a
fengdachao <fengdachao@aliyun.com>
2025-01-16 17:19:49 +0800

    change api server to env.HOST

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
+export const HOST = process.env.HOST || 'localhost'
 export const TITLE = '工程项目信息管理系统'
-export const HOST = '10.30.58.143'
 // export const HOST = 'localhost'
 // export const HOST = '100.168.1.21'
 // export const HOST = 'mongo-local'
